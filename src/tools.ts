468905093ad6cfe9b7de8e744fa0d9f2b4a2fa81
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-19 13:10:31 +0200

    finished

--- src/tools.ts
+++ src/tools.ts
@@ -1,10 +1,23 @@
 /**
  * Make the first character of a string uppercase.
  * 
- * const title = capitalizeFirstLetter("robert");
+ * const title = tools.formatWord("robert");
  * 
  * Robert
  */
-export const capitalizeFirstLetter = (text: string) => {
-	return text.charAt(0).toUpperCase() + text.slice(1); 
-}
+export const formatWord = (title: string) => {
+  const trimmedTitle = title.trim();
+  const _title =
+    trimmedTitle.slice(0, 1).toUpperCase() +
+    trimmedTitle.slice(1).toLowerCase();
+  return _title;
+};
+
+/**
+ * Adds two numbers and returns the result.
+ * 
+ * const sum = tools.sume(4,5);
+ * 
+ * 9
+ */
+export const sume = (a:number, b: number) => a + b;
