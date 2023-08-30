47d6b84c62a2e3fda02f70affad1c9cb56e754ee
AlbertLeipzig <albertclemvill@gmail.com>
2023-08-30 09:55:40 +0200

    fertig?

--- src/components/header.ts
+++ src/components/header.ts
@@ -2,6 +2,7 @@ import booksData from '../../book-data.json';
 const url = 'http://localhost:5173/';
 
 const likedBooks = booksData.slice(3, 10);
+
 console.log(likedBooks);
 
 // it must show a number related to the liked books length
@@ -16,12 +17,13 @@ export const header = () => {
   <header>
     <img src="../../public/images/company-logo.png" alt="company logo" class="company-logo--small">
     <input type="text" placeholder="Search" id="inputField" onchange="(console.log(typeof parseInt(this.value)))">
+    
     <ul class="icon-container">
       
       <li><a href=""><img src="../../public/images/user.png" alt="user page link" class="social_icon"></a></li>
-      <li><img src="../../public/images/heart.png" alt="like this book" class="social_icon"><p>${
-        likedBooks.length
-      }</p></li>
+      <li><img src="../../public/images/heart.png" alt="like this book" class="social_icon">
+      ${likedBooks.length >= 1 ? `<p>${likedBooks.length}</p>` : ""}
+      </li>
       <li><a href=""><img src="../../public/images/shopping-cart.png" alt="shopping cart link" class="social_icon"></a></li>
     </ul>
     <nav>
