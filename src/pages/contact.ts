b07a45c442dce496c3cc4874d4cb61c703b4071c
AlbertLeipzig <albertclemvill@gmail.com>
2023-09-06 11:37:39 +0200

    added test to required inputs

--- src/pages/contact.ts
+++ src/pages/contact.ts
@@ -1,18 +1,79 @@
 export const contact_page = () => {
-  return  /*html*/`
+  let formData = { firstName: '', lastName: '', email: '', message: '' };
+
+  const handleFormData = () => {
+    if (
+      formData.firstName.length < 1 ||
+      formData.lastName.length < 1 ||
+      formData.email.length < 1
+    ) {
+      const error = 'There are empty fields';
+      console.error(error);
+      return error;
+    } else {
+      console.log(formData);
+    }
+  };
+
+  window.addEventListener('load', () => {
+    const form = document.querySelector('form');
+    const firstNameLabel = document.createElement('label');
+    const lastNameLabel = document.createElement('label');
+    const emailLabel = document.createElement('label');
+    const firstNameInput = document.createElement('input');
+    const lastNameInput = document.createElement('input');
+    const emailInput = document.createElement('input');
+    const messageLabel = document.createElement('label');
+    const messageInput = document.createElement('textArea');
+    const submitButton = document.createElement('button');
+    firstNameLabel.innerText = 'First Name';
+    lastNameLabel.innerText = 'Last Name';
+    emailLabel.innerText = 'email';
+    submitButton.innerText = 'Send';
+    submitButton.type = 'button';
+    firstNameLabel.appendChild(firstNameInput);
+    lastNameLabel.appendChild(lastNameInput);
+    emailLabel.appendChild(emailInput);
+    messageLabel.appendChild(messageInput);
+    form?.appendChild(firstNameLabel);
+    form?.appendChild(lastNameLabel);
+    form?.appendChild(emailLabel);
+    form?.appendChild(messageLabel);
+    form?.appendChild(submitButton);
+    firstNameInput.addEventListener('change', (e: Event) => {
+      const target = e.target as HTMLInputElement;
+      formData.firstName = target.value;
+    });
+    lastNameInput.addEventListener('change', (e: Event) => {
+      const target = e.target as HTMLInputElement;
+      formData.lastName = target.value;
+    });
+    emailInput.addEventListener('change', (e: Event) => {
+      const target = e.target as HTMLInputElement;
+      formData.email = target.value;
+    });
+    messageInput.addEventListener('change', (e: Event) => {
+      const target = e.target as HTMLInputElement;
+      formData.message = target.value;
+    });
+    submitButton.addEventListener('click', () => handleFormData());
+  });
+
+  /* const checkInput = () => {
+        const inputValue = input.value
+        if(inputValue !== "") {
+            return 
+        }
+    } */
+
+  // const formData = {firstName : "", lastName : "", email : ""}
+
+  // const handleFormData = () => {}
+
+  // const sendFormData = () => {}
+
+  return /*html*/ `
         <form>
-            <label>First Name
-                <input type="text" placeholder = "Friedrich" ></input>
-            </label>
-            <label>Last Name
-                <input type="text" placeholder = "Schäffer"></input>
-            </label>
-            <label>Email
-                <input type="email" placeholder = "myemail@mail.com"></input>
-            </label>
-            <label>Message
-                <textarea>Please, your message here</textarea>
-            </label>
         </form>
     `;
 };
